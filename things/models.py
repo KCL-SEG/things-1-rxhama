6c92ab33107ec80498a994097c6201f7f4712a7a
Roland Xhama <rxhama02@gmail.com>
2022-12-09 11:18:21 +0000

    Fix

--- things/models.py
+++ things/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    name =
+    name = models.CharField(max_length=35, unique=True)
+    description = models.CharField(max_length=120, blank=True)
+    quantity = models.IntegerField(
+        validators=[MinValueValidator(0),MaxValueValidator(50)]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
